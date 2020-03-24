8cee472ff04892c54999819880a90e1a21d9cd9d
Petros Fountas <petros@polymitis.com>
2020-03-24 23:44:18 +0000

    Fix Navigate Plane pinch zoom, add pinch rotate, and move cube
    The Navigate Plane scene is complete. On start, a board made of a
    16-tile perimeter is placed on the closest detected AR plane. On
    touch and hold at a board tile, a cube is placed on the tile
    center. Dragging the cube or touching and holding on another tile
    will move the cube around the tiles of the perimeter. The board
    can be pulled towards the camera or pushed further away with
    pinch zoom. Pinch rotate will rotate the board around its center.
    
    Changes:
     * Implemented "debouncing" control for distinguishing different
       haptics.
     * Zoom is done by +/- the
       m_SessionOrigin.transform.localPosition
     * The Plane is instantiated on a detected AR Plane.
     * Plane rotation is done by +/- the
       plane.transform.rotation.eulerAngles.y
     * The Cube is instantiated and moved on ray traced objects in
       the scene, i.e. the board tiles.

--- Assets/Scripts/NavigatePlane.cs
+++ Assets/Scripts/NavigatePlane.cs
@@ -3,110 +3,191 @@ using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
-/// <summary>
-/// Listens for touch events and performs an AR raycast from the screen touch point.
-/// AR raycasts will only hit detected trackables like feature points and planes.
-///
-/// If a raycast hits a trackable, the <see cref="placedPrefab"/> is instantiated
-/// and moved to the hit position.
-/// </summary>
 [RequireComponent(typeof(ARRaycastManager))]
 public class NavigatePlane : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Instantiates this prefab on a plane at the touch location.")]
-    GameObject m_PlacedPrefab;
+    GameObject m_PlanePrefab;
 
-    /// <summary>
-    /// The prefab to instantiate on touch.
-    /// </summary>
-    public GameObject placedPrefab
+    public GameObject planePrefab
     {
-        get { return m_PlacedPrefab; }
-        set { m_PlacedPrefab = value; }
+        get { return m_PlanePrefab; }
+        set { m_PlanePrefab = value; }
     }
 
-    /// <summary>
-    /// The object instantiated as a result of a successful raycast intersection with a plane.
-    /// </summary>
-    public GameObject spawnedObject { get; private set; }
+    public GameObject plane { get; private set; }
+
+    [SerializeField]
+    [Tooltip("Instantiates this prefab on a plane at the touch location.")]
+    GameObject m_CubePrefab;
+
+    public GameObject cubePrefab
+    {
+        get { return m_CubePrefab; }
+        set { m_CubePrefab = value; }
+    }
+
+    public GameObject cube { get; private set; }
 
     void Awake()
     {
         m_RaycastManager = GetComponent<ARRaycastManager>();
+        m_SessionOrigin = GetComponent<ARSessionOrigin>();
+        m_Camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
-    bool TryGetTouchPosition(out Vector2 position)
+    bool TryAcceptingHaptic(in HapticType type)
     {
-        if (Input.touchCount > 0)
+        if (m_CanditateHapticControl != type)
+            m_NumHaptics = 0;
+
+        m_CanditateHapticControl = type;
+
+        if (m_PreviousHapticControl != type && (m_NumHaptics = ++m_NumHaptics % m_ValidMinHaptics) == 0)
+            m_PreviousHapticControl = type;
+
+        return (m_PreviousHapticControl == type);
+    }
+
+    bool TryGetTouch(out Vector2 position)
+    {
+        if (Input.touchCount == 1)
         {
             position = Input.GetTouch(0).position;
-            return true;
+
+            return TryAcceptingHaptic(HapticType.Touch);
         }
 
         position = default;
         return false;
     }
 
-    bool TryGetPinchDistance(out float zoom)
+    bool TryGetPinch(out float zoom, out float angle, out Vector2 center)
     {
-        if (Input.touchCount > 1)
+        if (Input.touchCount == 2)
         {
-            Debug.Log("NavigatePlane: Pinch detected");
-            
             var touch0 = Input.GetTouch(0).position;
             var touch1 = Input.GetTouch(1).position;
             var dist0 = Vector2.Distance(touch0, touch1);
-            Debug.Log("NavigatePlane: Pinch 0 distance " + dist0);
+            center = (touch0 + touch1) / 2;
 
             var delta0 = Input.GetTouch(0).deltaPosition;
             var delta1 = Input.GetTouch(1).deltaPosition;
+            var angle0 = 2 * Mathf.Atan2(Vector2.Distance(touch0, touch0 - delta0), Vector2.Distance(center, touch0));
+            var angle1 = 2 * Mathf.Atan2(Vector2.Distance(touch1, touch1 - delta1), Vector2.Distance(center, touch1));
+            angle = Mathf.FloorToInt((angle0 + angle1) * 100);
+            angle = (angle > 5) ? angle : 0;
+            if (touch0.x < (Screen.width / 2))
+                angle = (touch0.y > (touch0 - delta0).y) ? angle : -angle;
+            else
+                angle = (touch0.y < (touch0 - delta0).y) ? angle : -angle;
+
             var dist1 = Vector2.Distance(touch0 - delta0, touch1 - delta1);
-            Debug.Log("NavigatePlane: Pinch -1 distance " + dist1);
+            zoom = (Mathf.Abs(dist0 - dist1) > 20) ? dist0 - dist1 : 0;
 
-            zoom = dist0 - dist1;
-            Debug.Log("NavigatePlane: Pinch zoom " + zoom);
-
-            return true;
+            return TryAcceptingHaptic(HapticType.Pinch);
         }
 
+        angle = default;
+        center = default;
         zoom = default;
         return false;
     }
 
     void Update()
     {
-        if (TryGetTouchPosition(out Vector2 position))
+        if (!plane)
         {
-            if (m_RaycastManager.Raycast(position, m_Hits, TrackableType.PlaneWithinPolygon))
+            Vector2 screenCenter;
+            screenCenter.x = Screen.width / 2;
+            screenCenter.y = Screen.height / 2;
+
+            if (m_RaycastManager.Raycast(screenCenter, m_Hits, TrackableType.PlaneWithinPolygon))
             {
-                // Raycast hits are sorted by distance, so the first one
-                // will be the closest hit.
                 var hitPose = m_Hits[0].pose;
 
-                if (spawnedObject == null)
+                plane = Instantiate(m_PlanePrefab, hitPose.position, hitPose.rotation);
+                m_SessionOrigin.MakeContentAppearAt(plane.transform,
+                    plane.transform.position,
+                    plane.transform.rotation);
+                Debug.Log("NavigatePlane: Plane instantiated at " + hitPose.position);
+            }
+        }
+
+        if (TryGetTouch(out Vector2 touchPos))
+        {
+            var ray = m_Camera.ScreenPointToRay(touchPos);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            {
+                var position = new Vector3(hit.transform.position.x, plane.transform.position.y, hit.transform.position.z);
+
+                if (!cube)
                 {
-                    spawnedObject = Instantiate(m_PlacedPrefab, hitPose.position, hitPose.rotation);
+                    cube = Instantiate(cubePrefab, position, Quaternion.identity, plane.transform);
+                    Debug.Log("NavigatePlane: Cube instantiated at " + cube.transform.position);
+                }
+                else
+                {
+                    cube.transform.position = position;
+                    Debug.Log("NavigatePlane: Cube moved at " + cube.transform.position);
                 }
             }
         }
 
-        if (TryGetPinchDistance(out float zoom))
+        if (TryGetPinch(out float pinchZoom, out float pinchAngle, out Vector2 pinchCenter))
         {
-            if (zoom > 0f)
+            var ray = m_Camera.ScreenPointToRay(pinchCenter);
+            
+            if (pinchZoom > 0f)
             {
-                Camera.main.transform.Translate(0, 0, 1 * m_zoomSpeed, Space.World);
-                Debug.Log("NavigatePlane: Zoom in " + Camera.main.transform.localPosition);
+                m_SessionOrigin.transform.localPosition += ray.direction * m_zoomSpeed;
+                Debug.Log("NavigatePlane: Zoom in " + m_SessionOrigin.transform.localPosition);
             }
-            else if (zoom < 0f)
+            else if (pinchZoom < 0f)
             {
-                Camera.main.transform.Translate(0, 0, -1 * m_zoomSpeed, Space.World);
-                Debug.Log("NavigatePlane: Zoom out " + Camera.main.transform.localPosition);
+                m_SessionOrigin.transform.localPosition -= ray.direction * m_zoomSpeed;
+                Debug.Log("NavigatePlane: Zoom out " + m_SessionOrigin.transform.localPosition);
+            }
+            else if (pinchAngle > 0f)
+            {
+                var angle = plane.transform.rotation.eulerAngles.y + m_angularSpeed;
+                plane.transform.rotation = Quaternion.Euler(0, angle, 0);
+                Debug.Log("NavigatePlane: Rotate +Y at angle " + plane.transform.rotation.eulerAngles.y);
+            }
+            else if (pinchAngle < 0f)
+            {
+                var angle = plane.transform.rotation.eulerAngles.y - m_angularSpeed;
+                plane.transform.rotation = Quaternion.Euler(0, angle, 0);
+                Debug.Log("NavigatePlane: Rotate -Y at angle " + plane.transform.rotation.eulerAngles.y);
             }
         }
     }
 
+    private static readonly float m_angularSpeed = 2f;
+
+    private Camera m_Camera;
+
     private static readonly List<ARRaycastHit> m_Hits = new List<ARRaycastHit>();
+
+    private static int m_NumHaptics = 0;
+    private static readonly int m_ValidMinHaptics = 10;
+
+    private enum HapticType
+    {
+        None = 0,
+        Touch,
+        Pinch,
+    };
+
+    private static HapticType m_CanditateHapticControl = HapticType.None;
+    private static HapticType m_PreviousHapticControl = HapticType.None;
+
     private ARRaycastManager m_RaycastManager;
-    private static readonly float m_zoomSpeed = 1.01f;
+
+    private ARSessionOrigin m_SessionOrigin;
+
+    private static readonly float m_zoomSpeed = 0.05f;
 }
